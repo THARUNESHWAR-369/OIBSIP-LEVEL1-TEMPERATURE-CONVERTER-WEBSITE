4b07b25c4a33b0cdb07d553f57c2a42ea53ba3d9
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2023-08-19 18:42:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function convertTemperature() {
+  const temperatureInput = parseFloat(
+    document.getElementById("temperatureInput").value
+  );
+  const unitSelect = document.getElementById("unitSelect").value;
+  let convertedTemperature;
+  let convertedUnit;
+
+  if (isNaN(temperatureInput)) {
+    document.getElementById("result").innerText =
+      "Please enter a valid number.";
+    return;
+  }
+
+  if (unitSelect === "celsius") {
+    convertedTemperature = temperatureInput;
+    convertedUnit = "°C";
+  } else if (unitSelect === "fahrenheit") {
+    convertedTemperature = (temperatureInput * 9) / 5 + 32;
+    convertedUnit = "°F";
+  } else if (unitSelect === "kelvin") {
+    convertedTemperature = temperatureInput + 273.15;
+    convertedUnit = "K";
+  }
+
+  document.getElementById(
+    "result"
+  ).innerText = `Converted Temperature: ${convertedTemperature.toFixed(
+    2
+  )} ${convertedUnit}`;
+}
